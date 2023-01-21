bc9aca65eb3a7d1775485b63fab9ccd83dccd858
gwenparadis <gwendelynparadis2@gmail.com>
2023-01-21 14:35:55 -0500

    cleaned up models index

--- models/index.js
+++ models/index.js
@@ -1,46 +1,12 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
-Product.belongsToMany(Tag, {
-  // Define the third table needed to store the foreign keys
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'product_tag'
-});
-
-Tag.belongsToMany(Product, {
-  // Define the third table needed to store the foreign keys
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'tag_product'
-});
-
-//category has many products
-Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
-});
-
-// A product belongs to a single category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
+  foreignKey: "category_id",
 });
 
 module.exports = {
@@ -49,3 +15,31 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  // Define an alias for when data is retrieved
+  as: "product_tag",
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  // Define an alias for when data is retrieved
+  as: "tag_product",
+});
+
+//category has many products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
